871d0025d058c2771c3381547791d3074b19e4c5
Ss168638 <87580057+Ss168638@users.noreply.github.com>
2022-03-26 02:00:44 +0530

    Snake Game Fully Functional
    Added Grid, Snake Body, Food and other logics.

--- main.py
+++ main.py
@@ -1,137 +1,172 @@
 import pygame
 import random
-from pygame.locals import *
-
-
-#init pygame
+#initializing pygame
 pygame.init()
 
-WIDTH , HEIGHT = 400, 400
-width, height = 20, 20
+width , height = 400, 400
+FRAME_RATE = 3
+SCALE = 20
+iterator = []   #iterator for Grid Points
+#storing random values to generate snake food
+for value in range(0, (width//SCALE)*(height//SCALE)):
+    iterator.append(random.randint(1, (width//SCALE)*(height//SCALE)-SCALE))
 
-FRAME_RATE = 30
+#setting the window size
+WIN = pygame.display.set_mode((width, height))
 
-BG_COLOR = (0, 0, 0)
-RECT_COLOR = (0, 255, 0)
-
-
-
-
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-# RECT_BOX = pygame.Rect(left, top, width, height)
-
-class FOOD:
-    def __init__(self, eaten):
-        self.eaten = eaten
-        self.food_x = random.randint(0, WIDTH)
-        self.food_y = random.randint(0, HEIGHT)
-        self.food = (self.food_x, self.food_y)
-        self.FOOD_SIZE = 10
-        self.FOOD_COLOR = (255, 0, 0)
-
-    def generate_food(self, win):
-        if self.eaten == 1:
-            self.food_x = random.randint(0, WIDTH)
-            self.food_y = random.randint(0, HEIGHT)
-            self.food = (self.food_x, self.food_y)
-        pygame.draw.circle(win, self.FOOD_COLOR, self.food, self.FOOD_SIZE)
-
-
-
+#Snake Class
 class SNAKE:
-    def __init__(self, x, y):
+    def __init__(self, x, y, xwidth, yheight):
         self.x = x
         self.y = y
-    
-    def draw_rect(self, win):
-        self.rect = pygame.Rect(self.x, self.y, width, height)
-        pygame.draw.rect(win, RECT_COLOR, self.rect)
-    
-    def move_down(self, win, speed):
-        if self.y == WIDTH:
+        self.xwidth = xwidth
+        self.yheight = yheight
+        self.total = 0
+        self.color = (255, 0, 0)
+        self.foodcolor = (0, 0, 255)
+        self.tail = []
+        self.foodx = 0
+        self.foody = 0
+        self.foodwidth = 0
+        self.foodheight = 0
+        self.eaten = False
+        self.grid = []
+    #to draw snake head
+    def drawRect(self, win):
+        rect = pygame.Rect(self.x, self.y, self.xwidth, self.yheight)
+        pygame.draw.rect(win, self.color, rect)
+        win.blit(win, (0, 0))
+        pygame.display.flip()
+        if self.total > 0:
+            self.tail.append(rect)
+
+    def moveUP(self):
+        if self.y == 0:
+            self.y = height
+        else:
+            self.y -= SCALE
+
+    def moveLEFT(self):
+        if self.x == 0:
+            self.x = width
+        else:
+            self.x -= SCALE
+
+    def moveDown(self):
+        if self.y == height:
             self.y = 0
         else:
-            self.y += speed
-
-    def move_up(self, win, speed):
-        if self.y == 0:
-            self.y = HEIGHT
-        else:
-            self.y -= speed
-
-    def move_left(self, win, speed):
-        if self.x == 0:
-            self.x = WIDTH
-        else:
-            self.x -= speed
-
-    def move_right(self, win, speed):
-        if self.x == WIDTH:
+            self.y += SCALE
+            
+    def moveRIGHT(self):
+        if self.x == width:
             self.x = 0
         else:
-            self.x += speed
+            self.x += SCALE
+    #to draw snake food
+    def drawfood(self, win, value):
+        index = iterator[value]
+        self.foodx, self.foody, self.foodwidth, self.foodheight = self.grid[index]
+        rect = pygame.Rect(self.foodx, self.foody, self.foodwidth, self.foodheight)
+        pygame.draw.rect(win, self.foodcolor, rect)
+        win.blit(win, (0, 0))
+        pygame.display.flip()
 
+    #check for food eaten
+    def foodeaten(self):
+        snakerect = pygame.Rect(self.x, self.y, self.xwidth, self.yheight)
+        foodrect = pygame.Rect(self.foodx, self.foody, self.foodwidth, self.foodheight)
+        if snakerect.colliderect(foodrect) == True:
+            self.total += 1
+            return True
+        return False
+    #check snake head collision with body
+    def bodycollisioncheck(self):
+        if len(self.tail) > 4:
+            snakerect = pygame.Rect(self.x, self.y, self.xwidth, self.yheight)
+            for tail in self.tail[:-1]:
+                tailrect = pygame.Rect(tail[0], tail[1], tail[2], tail[3])
+                if snakerect.colliderect(tailrect) == True:
+                    print("Game Over")
+                    self.total = 0
+                    return True
+        return False
 
+    #drawing the Grid Layout
+    def drawgrid(self, surface):
+        size = 20
+        for rows in range(0, width//SCALE, 1):
+            for cols in range(0, height//SCALE, 1):
+                rect = pygame.Rect(rows*size, cols*size, size, size)
+                pygame.draw.rect(surface, (0, 255, 0), rect, 1)
+                self.grid.append(rect)
 
-def eventLoop():
+    #drawing body of snake
+    def drawTail(self, win):
+        for i in range(len(self.tail)-1):
+            self.tail[i] = self.tail[i+1]
+        self.tail = self.tail[:self.total]
+        for tail in self.tail:
+            pygame.draw.rect(win, (230, 120, 210), tail)
+            win.blit(win, (0, 0))
+            pygame.display.flip()
 
+#Main event Loop for Game
+def eventloop():
+    seed = 4
     run = True
-    count = 1
     clock = pygame.time.Clock()
-    snake = SNAKE(10, 10)
-    food = FOOD(False)
-    SPEED = 2
-    up, down, left, right = 0, 0, 0, 0
+    up, left, down, right = 0, 0, 0, 0
+    snake = SNAKE(0+SCALE, 0+SCALE, SCALE, SCALE)
+    grid_surf = pygame.Surface((width,height))
+    snake.drawgrid(grid_surf)
+    WIN.blit(grid_surf, (0, 0))
+    pygame.display.flip()
 
     while run:
         clock.tick(FRAME_RATE)
-        WIN.fill(BG_COLOR)
-        snake.draw_rect(WIN)
-        food.generate_food(WIN)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_DOWN:
-                    up, down, left, right = 0, 1, 0, 0
-                    # snake.move_down(WIN, SPEED)
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    up, down, left, right = 1, 0, 0, 0
-                    # snake.move_up(WIN, SPEED)
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    up, down, left, right = 0, 0, 1, 0
-                    # snake.move_left(WIN, SPEED)
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    up, down, left, right = 0, 0, 0, 1
-                    # snake.move_right(WIN, SPEED)
-        if down == 1:
-            snake.move_down(WIN, SPEED)
-            # SPEED += 1
-        elif up == 1:
-            snake.move_up(WIN, SPEED)
-            # SPEED += 1
-        elif left == 1:
-            snake.move_left(WIN, SPEED)
-            # SPEED += 1
-        elif right == 1:
-            snake.move_right(WIN, SPEED)
-            # SPEED += 1
-        else:
-            pass
 
-        if food.eaten == True:
-            food.generate_food(WIN)
-            food.eaten = False
-        snake.draw_rect(WIN)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP and down == False:
+                    up, left, down, right = 1, 0, 0, 0
+                    print("Snake moved up!")
+                elif event.key == pygame.K_LEFT and right == False:
+                    up, left, down, right = 0, 1, 0, 0
+                    print("Snake moved left!")
+                elif event.key == pygame.K_DOWN and up == False:
+                    up, left, down, right = 0, 0, 1, 0
+                    print("Snake moved down!")
+                elif event.key == pygame.K_RIGHT and left == False:
+                    up, left, down, right = 0, 0, 0, 1
+                    print("Snake moved right!")
+                    
+        snake.eaten = snake.foodeaten()
+        if (snake.eaten == True):
+            snake.eaten == False
+            seed += 3
+
+        if snake.bodycollisioncheck() == True:
+            run = False
+
+        if up == True:
+            snake.moveUP()
+        elif left == True:
+            snake.moveLEFT()
+        elif down == True:
+            snake.moveDown()
+        elif right == True:
+            snake.moveRIGHT()
+            
+        WIN.blit(grid_surf, (0, 0))
+        pygame.display.flip()
+        snake.drawfood(WIN, seed)
+        snake.drawTail(WIN)
+        snake.drawRect(WIN)
         pygame.display.update()
-
-
     pygame.quit()
 
-
 if __name__ == "__main__":
-    jls_extract_var = eventLoop
-    jls_extract_var()
+    eventloop()
